aa59ebb7e9739ce291f1cff11ab973ece848b720
Duc Le <ducle@nvg.vn>
2019-08-09 13:38:19 +0700

    init

---
+++ Headlines/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Headlines
+//
+//  Created by Mohammad Azam on 10/20/17.
+//  Copyright © 2017 Mohammad Azam. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    var app :App?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        if let window = window {
+            app = App(window: window)
+        }
+        
+        return true
+    }
+
+
+
+
+}
+
---
+++ Headlines/Controllers/AddArticleViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AddArticleViewController.swift
+//  Headlines
+//
+//  Created by Mohammad Azam on 10/20/17.
+//  Copyright © 2017 Mohammad Azam. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddArticleViewController : UIViewController {
+    
+    // For this example assume they are UITextField
+    @IBOutlet weak var titleTextField :BindingTextField! {
+        didSet {
+            titleTextField.bind { self.viewModel.title.value = $0 }
+        }
+    }
+    @IBOutlet weak var descriptionTextField :BindingTextField! {
+        didSet {
+            descriptionTextField.bind { self.viewModel.description.value = $0 }
+        }
+    }
+    
+    var viewModel :AddArticleViewModel! {
+       
+        didSet {
+            viewModel.title.bind = { [unowned self] in self.titleTextField.text = $0 }
+            viewModel.description.bind = { [unowned self] in self.descriptionTextField.text = $0 }
+        }
+    }
+    
+    @IBAction func AddArticleButtonPressed(_ sender: Any) {
+        
+//        self.viewModel.title.value = "hello world"
+//        self.viewModel.description.value = "description"
+        self.viewModel.didAddArticle?(self.viewModel?.title.value, self.viewModel?.description.value)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+    
+}
